d610bff47039458ea44efcf3e7b8327d6b4f4aaf
gakawarstone <pinnekerv@gmail.com>
2020-11-06 00:22:38 +0600

    53 commit

---
+++ deikstra/js/scripts.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+  function checkWidth() {
+    var windowWidth = $('body').innerWidth(),
+        elem = $(".socreq"); // лучше сохранять объект в переменную, многократно чтобы не насиловать
+                                    // страницу для поиска нужного элемента
+    if(windowWidth < 768){
+      elem.addClass('order-first');
+    }
+    else{
+      elem.removeClass('order-first');
+    }
+  }
+
+  checkWidth(); // проверит при загрузке страницы
+
+  $(window).resize(function(){
+    checkWidth(); // проверит при изменении размера окна клиента
+  });
+});
