1c7f30ba595fc58b8c4c3c1a89e87d1416a22f67
gakawarstone <pinnekerv@gmail.com>
2021-06-14 16:42:54 +0600

    75 commit

--- marafon_js/01-cards/app.js
+++ marafon_js/01-cards/app.js
@@ -5,6 +5,9 @@ for (const slide of slides) {
     clearActiveClasses()
 
     slide.classList.add('active');
+    if (slide.classList.contains('active')) {
+      changeBackground(slide)
+    }
   })
 }
 
@@ -12,4 +15,32 @@ function clearActiveClasses() {
   slides.forEach((slide) => {
     slide.classList.remove('active')
   })
+}
+
+function changeBackground(slide) {
+  document.querySelector('body').classList.remove('active-slide-mint')
+  document.querySelector('body').classList.remove('active-slide-ubuntu')
+  document.querySelector('body').classList.remove('active-slide-manjaro')
+  document.querySelector('body').classList.remove('active-slide-fedora')
+  document.querySelector('body').classList.remove('active-slide-arch')
+
+  if (slide.classList.contains('mint')) {
+    document.querySelector('body').classList.add('active-slide-mint')
+  }
+
+  if (slide.classList.contains('ubuntu')) {
+    document.querySelector('body').classList.add('active-slide-ubuntu')
+  }
+
+  if (slide.classList.contains('manjaro')) {
+    document.querySelector('body').classList.add('active-slide-manjaro')
+  }
+
+  if (slide.classList.contains('fedora')) {
+    document.querySelector('body').classList.add('active-slide-fedora')
+  }
+
+  if (slide.classList.contains('arch')) {
+    document.querySelector('body').classList.add('active-slide-arch')
+  }
 }
